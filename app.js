9e0546dc55e93f8baa3f2939ee540d907bf8a106
zero2-in <wowin21@gmail.com>
2022-04-09 03:39:11 +0900

    폴더 구조 최적화

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import home from './routes/home/index.js';
+import home from './src/routes/home/index.js';
 
 const app = express();
 
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static("./src/public"));
 app.use("/", home);
 
 app.get("/", (req, res) => {
-    res.sendFile("./public/index.html");
+    res.sendFile("index.html");
 });
 
 export default app;
