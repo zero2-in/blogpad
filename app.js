4616564c9d6de9ebdc1ca63fdd09ac7d4ad9e028
zero2-in <wowin21@gmail.com>
2022-04-09 03:33:18 +0900

    app listen 모듈화

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import home from './routes/home/index.js';
 
 const app = express();
-const PORT = 3000;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -10,10 +9,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/", home);
 
-app.listen(PORT, () => {
-    console.log(`Server on ${PORT}`);
-});
-
 app.get("/", (req, res) => {
     res.sendFile("./public/index.html");
 });
+
+export default app;
