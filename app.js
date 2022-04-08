a8d9e38b901497f48908d938e6d5b051b7632330
zero2-in <wowin21@gmail.com>
2022-04-09 03:27:13 +0900

    라우팅 분리

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import home from './routes/home/index.js';
+
 const app = express();
 const PORT = 3000;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
+app.use("/", home);
 
 app.listen(PORT, () => {
     console.log(`Server on ${PORT}`);
